bedfb10f59ce3d517eb5d1b7a8c32dde9fc007ab
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-04 01:29:19 +0800

    Delete installer directory

--- installer/templates/phx_umbrella/apps/app_name/config/config.exs
+++
@@ -1,4 +0,0 @@
-<%= if @namespaced? || @ecto do %># Configure Mix tasks and generators
-config :<%= @app_name %><%= if @namespaced? do %>,
-  namespace: <%= @app_module %><% end %><%= if @ecto do %>,
-  ecto_repos: [<%= @app_module %>.Repo]<% end %><% end %>
