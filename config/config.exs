5af703cc6ba7b6ababd993919861d4248f9140cb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-04 01:28:42 +0800

    Delete config directory

--- config/config.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :logger, :console, colors: [enabled: false]
-
-config :phoenix, :stacktrace_depth, 20
-
-config :phoenix, :json_library, Jason
-
-config :phoenix, :trim_on_html_eex_engine, false
